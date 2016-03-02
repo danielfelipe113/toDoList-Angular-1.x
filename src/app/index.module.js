91aed3b558284a7a7e3ae49cdd4284a403d83930
Daniel Coy <danielfelipe113@gmail.com>
2016-03-02 15:13:43 -0500

    updated gh-pages

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('listaDeCosasPorHacerAngular1X', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
-
-})();
